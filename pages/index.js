e8b9185df1a8f3fcd89bf2cc25c7d763a65538a5
adityaMedpay <tiwariaditya15@hotmail.com>
2022-01-16 18:20:12 +0530

    fix: ssr flag

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,9 @@ import { useState } from "react";
 import dynamic from "next/dynamic";
 import "react-quill/dist/quill.snow.css";
 
-const Editor = dynamic(() => import("react-quill"));
+const Editor = dynamic(() => import("react-quill"), {
+  ssr: false,
+});
 export default function Home() {
   const [value, setValue] = useState("");
   console.log({ value });
