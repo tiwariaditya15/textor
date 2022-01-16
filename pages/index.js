d4c3b5cc4cde8e6d31410170590ac52328a310db
adityaMedpay <tiwariaditya15@hotmail.com>
2022-01-16 18:16:52 +0530

    fix: dynamic import

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import { useState } from "react";
-import Editor from "react-quill";
+import dynamic from "next/dynamic";
 import "react-quill/dist/quill.snow.css";
+
+const Editor = dynamic(() => import("react-quill"));
 export default function Home() {
   const [value, setValue] = useState("");
   console.log({ value });
