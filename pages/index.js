4c7a6728574f5fbd47eb08c3428e088c979f52fc
adityaMedpay <tiwariaditya15@hotmail.com>
2022-01-17 11:38:41 +0530

    feat: fetch/save from BE

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
-import { useEffect, useState } from "react";
+import { debounce, isError } from "lodash";
 import dynamic from "next/dynamic";
 import "react-quill/dist/quill.snow.css";
 import { useDispatch } from "react-redux";
 import { setValue as setEditorState } from "../features/editorSlice";
 import { useSelector } from "react-redux";
-import { useGetNoteQuery } from "../app/services/editorApi";
+import { useGetNoteQuery, useAddNoteMutation } from "../app/services/editorApi";
 
 const Editor = dynamic(() => import("react-quill"), {
   ssr: false,
@@ -14,17 +14,40 @@ export default function Home() {
   const value = useSelector((state) => state.editor.value);
   const dispatch = useDispatch();
   useGetNoteQuery();
-
+  const [addNote, { isLoading, isSuccess, isError }] = useAddNoteMutation();
+  console.log();
   return (
-    <section style={{}}>
-      <Editor
-        value={value}
-        onChange={(newState) => {
-          dispatch(setEditorState(newState));
+    <>
+      <section style={{}}>
+        <Editor
+          value={value}
+          onChange={async (newState) => {
+            dispatch(setEditorState(newState));
+            // debounce(async(await addNote(newState))(), 800);
+          }}
+          theme={"snow"}
+          style={{ height: "20rem" }}
+        />
+      </section>
+      <button
+        style={{
+          marginTop: "4rem",
+          marginLeft: "0.4rem",
+          padding: "1rem 1.5rem",
         }}
-        theme={"snow"}
-        style={{ height: "40rem" }}
-      />
-    </section>
+        onClick={async () => {
+          try {
+            await addNote(value);
+          } catch (error) {
+            console.log({ error });
+          }
+        }}
+      >
+        Submit
+      </button>
+      <p>{isLoading && "Saving..."}</p>
+      <p>{isSuccess && "Saved."}</p>
+      <p>{isError && "Couldn't save. Try again!"}</p>
+    </>
   );
 }
