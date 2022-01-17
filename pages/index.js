9baf94287b5316964449fa8983e5304ab7dae0af
adityaMedpay <tiwariaditya15@hotmail.com>
2022-01-17 15:12:33 +0530

    feat: more toolbar options

--- pages/index.js
+++ pages/index.js
@@ -1,31 +1,68 @@
-import { debounce, isError } from "lodash";
+import _ from "lodash";
 import dynamic from "next/dynamic";
 import "react-quill/dist/quill.snow.css";
 import { useDispatch } from "react-redux";
 import { setValue as setEditorState } from "../features/editorSlice";
 import { useSelector } from "react-redux";
 import { useGetNoteQuery, useAddNoteMutation } from "../app/services/editorApi";
+import { useEffect } from "react";
 
-const Editor = dynamic(() => import("react-quill"), {
-  ssr: false,
-});
+const toolbarOptions = [
+  ["bold", "italic", "underline", "strike"], // toggled buttons
+  ["blockquote", "code-block"],
+
+  [{ header: 1 }, { header: 2 }], // custom button values
+  [{ list: "ordered" }, { list: "bullet" }],
+  [{ script: "sub" }, { script: "super" }], // superscript/subscript
+  [{ indent: "-1" }, { indent: "+1" }], // outdent/indent
+  [{ direction: "rtl" }], // text direction
+
+  [{ size: ["small", false, "large", "huge"] }], // custom dropdown
+  [{ header: [1, 2, 3, 4, 5, 6, false] }],
+
+  [{ color: [] }, { background: [] }], // dropdown with defaults from theme
+  [{ font: [] }],
+  [{ align: [] }],
+
+  ["clean"], // remove formatting button
+];
+
+const Editor = dynamic(
+  () => {
+    const Editor = import("react-quill");
+    return Editor;
+  },
+  {
+    ssr: false,
+  }
+);
 
 export default function Home() {
   const value = useSelector((state) => state.editor.value);
   const dispatch = useDispatch();
   useGetNoteQuery();
   const [addNote, { isLoading, isSuccess, isError }] = useAddNoteMutation();
-  console.log();
+  console.log({ Editor });
+
   return (
     <>
       <section style={{}}>
         <Editor
           value={value}
-          onChange={async (newState) => {
-            dispatch(setEditorState(newState));
-            // debounce(async(await addNote(newState))(), 800);
-          }}
           theme={"snow"}
+          modules={{
+            toolbar: toolbarOptions,
+            clipboard: {
+              matchVisuals: false,
+            },
+          }}
+          onChange={(newState) => {
+            dispatch(setEditorState(newState));
+            // _.debounce(async function () {
+            //   console.log("debounced");
+            //   await addNote(newState);
+            // }, 800);
+          }}
           style={{ height: "20rem" }}
         />
       </section>
@@ -46,7 +83,14 @@ export default function Home() {
         Submit
       </button>
       <p>{isLoading && "Saving..."}</p>
-      <p>{isSuccess && "Saved."}</p>
+      <p
+        style={{
+          color: "Green",
+          padding: "1rem",
+        }}
+      >
+        {isSuccess && "Saved."}
+      </p>
       <p>{isError && "Couldn't save. Try again!"}</p>
     </>
   );
